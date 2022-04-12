f7d791b46c59b4b193af30a6c025793f04840e1d
Pranav Garimidi <prgarimidi@gmail.com>
2022-04-12 11:35:54 -0400

    Fixing properly displaying user's bets

--- bets/bets.py
+++ bets/bets.py
@@ -8,24 +8,13 @@ bets = f.Blueprint('bets', __name__, template_folder='templates/bets')
 def mybets():
     if f.g.user['uid'] == -1: return f.redirect(f.url_for('auth.login'))
     c = f.g.conn.execute(f"""
-                         SELECT Driver.name, T.teamName, Races.raceName, T.season, T.odds, T.wager, T.isOver
-                         FROM (SELECT *
-                               FROM (SELECT * 
-                                     FROM Bids 
-                                     WHERE uid='{f.g.user['uid']-2}'
-                                     ) T1 
-                                INNER JOIN Bet 
-                                    ON T1.betId = Bet.betId
-                                ) AS T
+                         SELECT Driver.name, T.teamName, Races.raceName, T.isover, T.place,  T.season, T.odds, T.wager, T.completed
+                         FROM ( SELECT * FROM BIDS NATURAL JOIN BET WHERE uid ={f.g.user['uid']}) AS T
                          INNER JOIN Races
                             ON T.raceId=Races.raceId
-                         INNER Join Driver
+                         left Join Driver
                             ON T.driverid=Driver.driverid
                         """)
-    c = f.g.conn.execute(f"""
-            SELECT *  FROM BIDS NATURAL JOIN BET WHERE uid='{f.g.user['uid']-2}'
-            """)
- 
     bet_types = {
         'driver_race': [],
         'driver_season': [],
@@ -34,15 +23,21 @@ def mybets():
     }
     find_type = lambda entry: tuple(i for i, val in enumerate(entry[:4]) if val!=None)
     for bet in c:
-        bet_type = find_type(bet) 
-        if bet_type == (0,2):
+        if bet[0] != None:
             bet_types['driver_race'].append(bet)
-        elif bet_type == (0,3):
-            bet_types['driver_season'].append(bet)
-        elif bet_type == (1,2):
-            bet_types['team_race'].append(bet)
         else:
-            bet_types['driver_season'].append(bet)
+            bet_types['team_race'].append(bet)
+
+
+       # bet_type = find_type(bet) 
+       # if bet_type == (0,2):
+       #     bet_types['driver_race'].append(bet)
+       # elif bet_type == (0,3):
+       #     bet_types['driver_season'].append(bet)
+       # elif bet_type == (1,2):
+       #     bet_types['team_race'].append(bet)
+       # else:
+       #     bet_types['driver_season'].append(bet)
 
     return f.render_template('bets/bets.html', **bet_types)
   
@@ -59,11 +54,11 @@ def placebet():
         try:
             bet = f.request.form
             f.g.conn.execute(f"""
-               INSERT INTO Bet (odds, isOver, place, raceID, teamName, driverId, completed)
+               INSERT INTO Bet (odds,isover, place, raceID, teamName, driverId, completed)
                VALUES
                (1.0, {bet["isOver"]}, {bet['position']}, {bet['race']},{bet['team']}, {bet['driver']}, FALSE); 
                INSERT INTO Bids 
-               VALUES ({f.g.user['uid']-2},currval('Bet_betid_seq'),{bet['betsize']}); 
+               VALUES ({f.g.user['uid']},currval('Bet_betid_seq'),{bet['betsize']}); 
                """)
         except Exception as e:
            print(e)
