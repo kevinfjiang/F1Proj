a98356a538af2dd532578e477f63c6ad55f21f11
kevinfjiang <kfj2112@columbia.edu>
2022-04-14 20:21:22 -0400

    added bet time validation

--- bets/makebet.py
+++ bets/makebet.py
@@ -1,6 +1,7 @@
 import flask as f
 import sqlalchemy
 from bets import bets
+import datetime
 import logging
 
 INTERNAL_DB_REGISTER_ERROR = "Database could not register\n Error:{}"
@@ -37,6 +38,14 @@ def create_informs_ent(form, betId):
     else:
         enter_informs_driver(form['driver'], raceId, betId)
 
+def check_prior(race:str)->bool:
+    resp = next(f.g.conn.execute("""
+                     SELECT stop
+                     FROM Races
+                     WHERE raceName=%s
+                     """, (race,)))
+    return resp!=None and resp>=datetime.datetime.now().date()
+
 @bets.route('/placebet', methods = ['GET','POST']) 
 def placebet():
     error=None
@@ -46,21 +55,24 @@ def placebet():
     if f.request.method == 'GET': 
        return f.render_template('bets/placebet.html', drivers = driver_list, teams = team_list, races = race_list)
     if f.request.method == 'POST':
-        try:
-            #TODO CHECK that the bet hasn't expired, super simple check
-            f.g.conn.execute(f"""
-               INSERT INTO Bet (odds, isOver, place, raceID, teamName, driverId, completed)
-               VALUES
-               (100, %(isOver)s, %(position)s, %(race)s, %(season)s, %(driver)s, FALSE); 
-               INSERT INTO Bids 
-               VALUES ({f.g.user['uid']}, currval('Bet_betid_seq'), %(betsize)s) 
-               """, f.request.form)
-            create_informs_ent(f.request.form, next(f.g.conn.execute("SELECT currval('Bet_betid_seq')")))
-        except sqlalchemy.exc.IntegrityError:
-            logging.log(logging.ERROR, "Repeated keys, double check new race is updated")
-        except Exception as e:
-           print(e)
-           error = INTERNAL_DB_REGISTER_ERROR.format(e)
+        if check_prior(f.request.form['race']):
+            error="Can't place a bet on a race that passed!"
+        else:
+            try:
+                #TODO CHECK that the bet hasn't expired, super simple check
+                f.g.conn.execute(f"""
+                INSERT INTO Bet (odds, isOver, place, raceID, teamName, driverId, completed)
+                VALUES
+                (100, %(isOver)s, %(position)s, %(race)s, %(season)s, %(driver)s, FALSE); 
+                INSERT INTO Bids 
+                VALUES ({f.g.user['uid']}, currval('Bet_betid_seq'), %(betsize)s) 
+                """, f.request.form)
+                create_informs_ent(f.request.form, next(f.g.conn.execute("SELECT currval('Bet_betid_seq')")))
+            except sqlalchemy.exc.IntegrityError:
+                logging.log(logging.ERROR, "Repeated keys, double check new race is updated")
+            except Exception as e:
+                print(e)
+                error = INTERNAL_DB_REGISTER_ERROR.format(e)
         if not error: return f.redirect(f.url_for('bets.mybets', outstanding=0)) 
         return f.render_template('bets/placebet.html', error=error, drivers = driver_list, teams = team_list, races = race_list)
 
